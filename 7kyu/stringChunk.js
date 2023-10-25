6b3143a30fef466243847c921421240cb9d32d01
AntonioDorosh <antondoroshenko92@gmail.com>
2023-10-25 18:42:51 +0300

    stringchunk solution

---
+++ 7kyu/stringChunk.js
@@ -0,0 +1,11 @@
+const stringChunk = (str, number) => {
+    if (number <= 0 || number === undefined || typeof number !== 'number') return [];
+    let result = [];
+    for (let i = 0; i < str.length; i += number) {
+        result.push(str.slice(i, i + number));
+    }
+    return result;
+};
+
+stringChunk('codewars', 2);
+stringChunk('thiskataeasy', 4)
