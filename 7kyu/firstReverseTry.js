5e603f9f289cc483f5a8e4acbd420429190f1271
AntonioDorosh <antondoroshenko92@gmail.com>
2023-10-27 14:25:35 +0300

    firstReverse solution

---
+++ 7kyu/firstReverseTry.js
@@ -0,0 +1,18 @@
+// const firstReverseTry = (arr) => {
+//     const firstEl = arr[0];
+//     const lastEl = arr[arr.length - 1];
+//
+//     if (arr.length === 0) return [];
+//     if (arr.length === 1) return arr;
+//
+//     arr[0] = lastEl;
+//     arr[arr.length - 1] = firstEl;
+//
+//     return arr;
+// };
+
+const firstReverseTry = (arr) => {
+    return arr.length > 1 ? arr.slice(-1).concat(arr.slice(1, -1)).concat(arr.slice(0, 1)) : arr;
+};
+
+firstReverseTry([23, 54, 12, 3, 4, 56, 23, 12, 5, 324]);
