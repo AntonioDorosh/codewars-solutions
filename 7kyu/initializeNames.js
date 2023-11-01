6b3ae01ab1231ed6a2eaed9d3d218a069207dd53
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-01 15:46:11 +0200

    second solution

--- 7kyu/initializeNames.js
+++ 7kyu/initializeNames.js
@@ -1,4 +1,4 @@
-const initializeNames = (name) => {
+const firstSolution = (name) => {
    const arr = name.split(' ');
 
     for (let i = 1; i < arr.length - 1; i++) {
@@ -6,6 +6,21 @@ const initializeNames = (name) => {
     }
 
     return arr.join(' ')
+};
+
+console.log(firstSolution('Los Mary Lane'))
+
+const initializeNames = (name) => {
+    const arr = name.split(' ');
+    let fullName = name;
+
+    if (arr.length > 2) {
+        let [firstName, lastName, middleName] = [arr.at(0), arr.at(-1), arr.slice(1, -1)];
+        let middleChange = middleName.map((val) => `${val.charAt(0)}.`).join(' ');
+        fullName = `${firstName} ${middleChange} ${lastName}`
+    }
+
+    return fullName
 };
 
 console.log(initializeNames('Los Mary Lane'))
