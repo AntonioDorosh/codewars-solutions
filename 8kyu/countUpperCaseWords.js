ac00de0a68c9113482891ef686273d23731431b5
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-02 20:24:33 +0200

    second solution

--- 8kyu/countUpperCaseWords.js
+++ 8kyu/countUpperCaseWords.js
@@ -1,15 +1,21 @@
+// const countUpperCaseWords = (array) => {
+//     let result = [];
+//
+//     for (let i = 0; i < array.length; i++) {
+//         const el = array[i];
+//
+//         if (el[0] === el[0].toUpperCase()) {
+//             result.push(el)
+//         }
+//     }
+//
+//     return result;
+// };
+//
+
+
 const countUpperCaseWords = (array) => {
-    let result = [];
-
-    for (let i = 0; i < array.length; i++) {
-        const el = array[i];
-
-        if (el[0] === el[0].toUpperCase()) {
-            result.push(el)
-        }
-    }
-
-    return result;
+    return array.filter((word) => word[0] === word[0].toUpperCase())
 };
 
 countUpperCaseWords(['New-York', 'London', 'moscow', 'Dubai'])
