2ddf429dead8f3ef1958920059dc92e2e160b736
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-04 13:31:12 +0200

    second solution

--- 7kyu/insertDash.js
+++ 7kyu/insertDash.js
@@ -1,5 +1,9 @@
-function insertDash(num) {
-    return num.toString().replace(/[13579](?=[13579])/g, '$&-')
-}
+// function insertDash(num) {
+//     return num.toString().replace(/[13579](?=[13579])/g, '$&-')
+// }
+
+const insertDash = (num) => {
+    return String(num).split('').map((v, index, array) => index !== 0 && array[index] % 2 !== 0 && array[index - 1] % 2 !== 0 ? `-${v}` : v).join('')
+};
 
 insertDash(454793)
