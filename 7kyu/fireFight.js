0e32482173c9fc56f1530416087dcde7a8272145
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-05 15:44:46 +0200

    easy solution for fireFight.js

---
+++ 7kyu/fireFight.js
@@ -0,0 +1,3 @@
+const fireFight = (str) => str.replaceAll('Fire', '~~');
+
+fireFight('Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast')  // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
