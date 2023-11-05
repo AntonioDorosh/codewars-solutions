e6af8e31aac5ef18d175a59f682e1a30602d4aa8
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-05 20:01:03 +0200

    findAvgAge.js solution

---
+++ 7kyu/findAvgAge.js
@@ -0,0 +1,51 @@
+const list1 = [
+    {
+        firstName: 'Maria',
+        lastName: 'Y.',
+        country: 'Cyprus',
+        continent: 'Europe',
+        age: 30,
+        language: 'Java'
+    },
+    {
+        firstName: 'Victoria',
+        lastName: 'T.',
+        country: 'Puerto Rico',
+        continent: 'Americas',
+        age: 70,
+        language: 'Python'
+    },
+];
+
+const list2 = [
+    {
+        firstName: 'Noa',
+        lastName: 'A.',
+        country: 'Israel',
+        continent: 'Asia',
+        age: 20,
+        language: 'Ruby'
+    },
+    {
+        firstName: 'Andrei',
+        lastName: 'E.',
+        country: 'Romania',
+        continent: 'Europe',
+        age: 21,
+        language: 'C'
+    },
+];
+
+const findAvgAge = (arr) => {
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i].age
+    }
+
+    return Math.round(result / arr.length)
+};
+
+findAvgAge(list1) // 50
+findAvgAge(list2) // 21
+
