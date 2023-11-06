adc2005ab029b59edfb85f1d22e65aead527a224
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-06 19:03:51 +0200

    absentVowel.js solution

---
+++ 7kyu/absentVowel.js
@@ -0,0 +1,23 @@
+const absentVowel = (str) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let result = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let lowerCase = str[i].toLowerCase();
+
+        if (vowels.includes(lowerCase)) {
+            result.push(lowerCase)
+        }
+    }
+
+    for (let i = 0; i < vowels.length; i++) {
+        const vowelsItems = vowels[i];
+
+        if (!result.includes(vowelsItems)) {
+            return vowels.indexOf(vowelsItems)
+        }
+    }
+};
+
+absentVowel("John Doe hs seven red pples under his bsket") // 0 --> missing: 'a'
+absentVowel('Bb Smith sent us six neatly arranged range bicycles') // 3 --> missing: 'o'
