0d4d8ae4cb13f5f665008cce85ceb0dbecec7821
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-08 16:00:58 +0200

    refactor

--- 7kyu/toJadenCase.js
+++ 7kyu/toJadenCase.js
@@ -2,3 +2,7 @@ String.prototype.toJadenCase = function () {
     return this.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ')
 }
 
+let str = 'How can mirrors be real if our eyes aren\'t real';
+
+str.toJadenCase() // output How Can Mirrors Be Real If Our Eyes Aren't Real
+
