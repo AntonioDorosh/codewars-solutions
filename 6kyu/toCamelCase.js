ee9f29603f9deae5177aa57294b49a40e53a1e92
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-08 12:55:19 +0200

    for and map solution

--- 6kyu/toCamelCase.js
+++ 6kyu/toCamelCase.js
@@ -1,7 +1,28 @@
+// for loop solution
+//
+// const toCamelCase = (str) => {
+//     let result = '';
+//
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === '_' || str[i] === '_') {
+//             result += str[i + 1].toUpperCase()
+//             i++;
+//         } else {
+//             result += str[i]
+//         }
+//     }
+//
+//     return result
+// };
+
+
+// map method solution
 const toCamelCase = (str) => {
-    const words = str.split(/[-_]/);
+    const regularFilter = /[_-]/g;
+    const words = str.split(regularFilter);
 
     return words.map((char, index) => index === 0 ? char : char[0].toUpperCase() + char.slice(1)).join('')
 };
+
 
 toCamelCase('the_stealth_warrior') // output theStealthWarrior
