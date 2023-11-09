cae997105545ec7b61229baae5e17d65b89d7592
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-09 11:52:15 +0200

    solution countLanguages

---
+++ 7kyu/countLanguages.js
@@ -0,0 +1,60 @@
+const list1 = [
+    {
+        firstName: 'Noah',
+        lastName: 'M.',
+        country: 'Switzerland',
+        continent: 'Europe',
+        age: 19,
+        language: 'C'
+    },
+    {
+        firstName: 'Anna',
+        lastName: 'R.',
+        country: 'Liechtenstein',
+        continent: 'Europe',
+        age: 52,
+        language: 'JavaScript'
+    },
+    {
+        firstName: 'Ramon',
+        lastName: 'R.',
+        country: 'Paraguay',
+        continent: 'Americas',
+        age: 29,
+        language: 'Ruby'
+    },
+    {
+        firstName: 'George',
+        lastName: 'B.',
+        country: 'England',
+        continent: 'Europe',
+        age: 81,
+        language: 'C'
+    },
+];
+
+// const countLanguages = (arr) => {
+//     let result = {};
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         if (result[arr[i].language] === undefined) {
+//             result[arr[i].language] = 1;
+//         } else {
+//             result[arr[i].language]++;
+//         }
+//     }
+//     return result;
+// };
+
+const countLanguages = (list) => {
+    return list.reduce((acc, obj) => {
+        if (acc[obj.language] === undefined) {
+            acc[obj.language] = 1;
+        } else {
+            acc[obj.language]++;
+        }
+        return acc;
+    }, {})
+};
+
+console.log(countLanguages(list1), { "C": 2, "Java": 1, "Ruby": 1 })
