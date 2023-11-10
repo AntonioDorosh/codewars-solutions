73eb5e3ce5da4fd26e3295f28f54c99721b7ea39
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-10 15:40:33 +0200

    reduce solution

--- 7kyu/sumTwoSmallest.js
+++ 7kyu/sumTwoSmallest.js
@@ -1,7 +1,15 @@
-const sumTwoSmallest = (num) => {
-    const [first, second] = num.sort((a, b) => a - b);
+// sort solution
 
-    return first + second;
+// const sumTwoSmallest = (num) => {
+//     const [first, second] = num.sort((a, b) => a - b);
+//
+//     return first + second;
+// };
+
+// reduce solution
+
+const sumTwoSmallest = (num) => {
+    return num.sort((a,b) => a - b).slice(0, 2).reduce((acc, item) => acc + item, 0)
 };
 
 console.log(sumTwoSmallest([5,8,12,19,22]), 13)
