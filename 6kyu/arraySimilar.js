29ece89047b39d0f64880f052e14c52507fe6b25
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-10 12:10:58 +0200

    arraySimilar.js solution

---
+++ 6kyu/arraySimilar.js
@@ -0,0 +1,11 @@
+const arr1 = [1, 2, 2, 3, 4],
+    arr2 = [2, 1, 2, 4, 3],
+    arr3 = [1, 2, 3, 4],
+    arr4 = [1, 2, 3, "4"];
+
+const arraySimilar = (arr1, arr2) => {
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort())
+};
+
+console.log(arraySimilar(arr1, arr2)) // true
+console.log(arraySimilar(arr3, arr4)) // false
