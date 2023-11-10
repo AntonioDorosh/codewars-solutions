18025edaf52edc3fd6b5d9e7952fa4fd35875d01
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-10 19:04:28 +0200

    scrollingText.js solution

---
+++ 7kyu/scrollingText.js
@@ -0,0 +1,21 @@
+// for loop solution
+
+// const scrollingText = (text) => {
+//     let result = [];
+//
+//     for (let i = 0; i < text.length; i++) {
+//         result.push((text.slice(i) + text.slice(0, i)).toUpperCase())
+//     }
+//
+//     return result;
+// };
+
+// map solution
+
+const scrollingText = (text) => {
+    return text.split('').map((char, index) => {
+        return text.slice(index).toUpperCase() + text.slice(0, index).toUpperCase()
+    })
+};
+
+console.log(scrollingText('abc'))
