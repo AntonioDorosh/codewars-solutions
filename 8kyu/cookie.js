b7eab1a2f5aad7aeb28d0794ae1f9f60dd4ca574
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-11 14:01:00 +0200

    cookie solution

---
+++ 8kyu/cookie.js
@@ -0,0 +1,10 @@
+const cookie = (x) => {
+    const type = typeof x;
+    let who = type === 'string' ? 'Zach' : type === 'number' ? 'Monica' : 'the dog';
+
+    return `Who ate the last cookie? It was ${who}`
+};
+
+console.log(cookie("Ryan"), "Who ate the last cookie? It was Zach!");
+console.log(cookie(2.3), "Who ate the last cookie? It was Monica!")
+console.log(cookie(true), "Who ate the last cookie? It was the dog!")
