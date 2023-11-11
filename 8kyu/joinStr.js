f0f8d6f9f2ee5c3439c0b258874ec56cf8b8290f
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-11 14:46:03 +0200

    easy task

---
+++ 8kyu/joinStr.js
@@ -0,0 +1,7 @@
+function joinStrings(string1, string2){
+    // code here
+    return `${string1} ${string2}`
+}
+
+console.log(joinStrings('string1', 'string2'), 'string1 string2')
+console.log(joinStrings('testing', 'testing'), 'testing testing')
