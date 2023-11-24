55af5ccdaf2aeda7a194ecb4887f10165668d408
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-24 21:15:14 +0200

    convertText.js sol

---
+++ 7kyu/convertText.js
@@ -0,0 +1,10 @@
+const convertText = (text) => {
+    const convert = text.split('').reverse().join('');
+    const firstChar = text.charAt(0);
+
+    return firstChar !== firstChar.toUpperCase() ? convert : text
+};
+
+
+console.log(convertText('hello'))
+console.log(convertText('Hello'))
