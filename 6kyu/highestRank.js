b106628f7536b35b804cd684e682fb813518f7a5
AntonioDorosh <antondoroshenko92@gmail.com>
2023-11-24 11:22:16 +0200

    highestRank.js sol

---
+++ 6kyu/highestRank.js
@@ -0,0 +1,20 @@
+
+const numbers = [12,10,8,12,7,6,4,10,12];
+
+const highestRank = (arr) => {
+    const obj = {};
+    arr.forEach((item) => {
+        if (obj[item]) {
+            obj[item] += 1
+        } else {
+            obj[item] = 1
+        }
+    })
+    
+    const max = Math.max(...Object.values(obj));
+    const result = Object.keys(obj).filter((item) => obj[item] === max);
+    
+    return Math.max(...result.map((item) => parseInt(item)))
+};
+
+console.log(highestRank(numbers), 12)
