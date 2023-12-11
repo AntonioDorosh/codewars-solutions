3c7ab417b6f303166f0d6693dbd4a2ebc1637793
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-11 16:16:18 +0200

    sortlist solution

---
+++ 7kyu/sortList.js
@@ -0,0 +1,11 @@
+const arr = [
+    {"a": 1, "b": 3},
+    {"a": 3, "b": 2},
+    {"a": 2, "b": 40},
+    {"a": 4, "b": 12}
+];
+
+const sortList = (sortBy, list) => {
+    return [...list].sort((a, b) => b[sortBy] - a[sortBy])
+};
+
