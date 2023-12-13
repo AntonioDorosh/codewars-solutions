7bcb50ad2530a817f50119a8a78b6457cfaebaef
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-13 15:18:30 +0200

    second sol

--- 7kyu/isSortedAndHow.js
+++ 7kyu/isSortedAndHow.js
@@ -1,6 +1,13 @@
+// const isSortedAndHow = (arr) => {
+//     return arr.every((item, index) => index === 0 || arr[index] >= arr[index - 1]) ? 'yes, ascending'
+//         : arr.every((item, index) => index === 0 || arr[index] <= arr[index - 1]) ? 'yes, descending' : 'no'
+// };
+
 const isSortedAndHow = (arr) => {
-    return arr.every((item, index) => index === 0 || arr[index] >= arr[index - 1]) ? 'yes, ascending'
-        : arr.every((item, index) => index === 0 || arr[index] <= arr[index - 1]) ? 'yes, descending' : 'no'
+    const ascending = arr.filter((el, index, array) => el > array[index + 1]).length === 0;
+    const descending = arr.filter((el, index, array) => el < array[index + 1]).length === 0;
+
+    return ascending ? 'yes, ascending' : descending ? 'yes, descending' : 'no'
 };
 
 console.log(isSortedAndHow([1, 2]), 'yes, ascending');
