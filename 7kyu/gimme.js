6d87fd3cc1feaa2e9ab005aa6097ded682558878
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-13 19:19:02 +0200

    second sol

--- 7kyu/gimme.js
+++ 7kyu/gimme.js
@@ -1,5 +1,11 @@
-function gimme (triplet) {
-    return [...triplet].indexOf(triplet.sort((a, b) => a - b)[1])
-}
+// function gimme (triplet) {
+//     return [...triplet].indexOf(triplet.sort((a, b) => a - b)[1])
+// }
+
+const gimme = (arr) => {
+    const existEl = [...arr].sort((a, b) => a - b);
+
+    return arr.indexOf(existEl[1])
+};
 
 console.log(gimme([5,10,14]), 1)
