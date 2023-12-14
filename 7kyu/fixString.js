5e4aef97e981024ffdcf1aadf06a0bfc91aff631
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-14 12:08:51 +0200

    reduce sol

--- 7kyu/fixString.js
+++ 7kyu/fixString.js
@@ -1,9 +1,15 @@
-const fixString = (str) => {
-    const arrChars = str.split('');
-    const upper = arrChars.filter((char) => char.match(/[A-Z]/)).length;
-    const lower = arrChars.filter((char) => char.match(/[a-z]/)).length;
+// const fixString = (str) => {
+//     const arrChars = str.split('');
+//     const upper = arrChars.filter((char) => char.match(/[A-Z]/)).length;
+//     const lower = arrChars.filter((char) => char.match(/[a-z]/)).length;
+//
+//     return lower >= upper ? str.toLowerCase() : str.toUpperCase()
+// };
 
-    return lower >= upper ? str.toLowerCase() : str.toUpperCase()
+// reduce method
+
+const fixString = (str) => {
+    return str.split('').reduce((acc, currentValue) => currentValue === currentValue.toUpperCase() ? acc + 1 : acc - 1, 0) > 0 ? str.toUpperCase() : str.toLowerCase()
 };
 
 
