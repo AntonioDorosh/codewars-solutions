a0b795d1c86e03db18ef97db39cfe6159a76edde
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-18 15:51:31 +0200

    sort sol

--- 7kyu/findLongest.js
+++ 7kyu/findLongest.js
@@ -2,4 +2,10 @@ const findLongest = (arr) => {
     return arr.reduce((prev, current) => (`${current}`.length > `${prev}`.length ? current : prev))
 };
 
+
+// sol with sort
+// const findLongest = (array) => {
+//     return array.sort((a, b) => String(b).length - String(a).length)[0]
+// };
+
 console.log(findLongest([8, 900, 500]));
