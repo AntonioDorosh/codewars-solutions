4359998df7c179334a0dbca74ff6574e60ec901d
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-18 19:25:15 +0200

    reduce sol

--- 8kyu/remove.js
+++ 8kyu/remove.js
@@ -1,11 +1,25 @@
-const remove = (str, n) => {
-    return str.split('').map((item) => {
-        if (n > 0 && item === '!') {
-            n--;
-            return ''
+// const remove = (str, n) => {
+//     return str.split('').map((item) => {
+//         if (n > 0 && item === '!') {
+//             n--;
+//             return ''
+//         }
+//         return item
+//     }).join('')
+// };
+
+// reduce method
+
+const remove = (str, num) => {
+    const mark = '!';
+
+    return [...str].reduce((acc, currentValue) => {
+        if (num > 0 && currentValue === mark) {
+            num--;
+            return acc;
         }
-        return item
-    }).join('')
-};
+        return acc + currentValue
+    }, '')
+}
 
 console.log(remove('Hi!', 1), 'Hi')
