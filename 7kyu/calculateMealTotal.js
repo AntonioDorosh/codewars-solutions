fa4ae9e9da1d2d8818d48c4d7a6b703dec733833
AntonioDorosh <antondoroshenko92@gmail.com>
2023-12-20 14:24:01 +0200

    mealTotal sol

--- 7kyu/calculateMealTotal.js
+++ 7kyu/calculateMealTotal.js
@@ -1,6 +1,5 @@
 const calculateTotal = (subtotal, tax, tip) => {
-    const total = subtotal + (subtotal * (tax / 100)) + (subtotal * (tip / 100));
-    return Math.round(total * 100) / 100
+    return Math.round(subtotal * (100 + tax + tip)) / 100
 };
 
 console.log(calculateTotal(5, 5, 10), 5.75)
