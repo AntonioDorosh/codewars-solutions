e6edf61d8c483e2137e81a5a227dc2aac53b55b1
AntonioDorosh <antondoroshenko92@gmail.com>
2024-01-02 13:45:36 +0200

    arrCheck sol

---
+++ 7kyu/arrCheck.js
@@ -0,0 +1,7 @@
+const arrCheck = (value) => {
+    return value.every(Array.isArray)
+};
+
+console.log(arrCheck([]), true)
+console.log(arrCheck(['string']), true)
+console.log(arrCheck([], {}), false)
