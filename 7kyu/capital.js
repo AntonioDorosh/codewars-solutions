2461722037e226f26ebe17c48663d760ecc2a976
AntonioDorosh <antondoroshenko92@gmail.com>
2024-01-04 16:15:48 +0200

    capital.js

---
+++ 7kyu/capital.js
@@ -0,0 +1,8 @@
+const findCap = (arr) => arr.map(({country, capital}) => `The capital ${country} is ${capital}`);
+
+console.log(findCap([
+    {
+        country: 'Spain',
+        capital: 'Madrid'
+    }
+]))
