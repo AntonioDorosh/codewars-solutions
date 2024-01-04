a8101013e93e0f9de8de06ad210923d0b23c808d
AntonioDorosh <antondoroshenko92@gmail.com>
2024-01-04 19:03:49 +0200

    couple sol

--- 7kyu/filterLucky.js
+++ 7kyu/filterLucky.js
@@ -1,4 +1,34 @@
-const filterLucky = (arr, num) => arr.filter((el) => el.toString().includes(num));
+// filter solution
+// const filterLucky = (arr, num) => arr.filter((el) => el.toString().includes(num));
+
+// reduce solution
+
+const filterLucky = (arr, target) => {
+    return arr.reduce((acc, currentValue) => {
+        const strArr = currentValue.toString();
+
+        if (strArr.includes(target)) {
+            acc.push(currentValue)
+        }
+
+        return acc
+    }, [])
+};
+
+// forEach sol
+
+// const filterLucky = (arr, target) => {
+//     const result = [];
+//
+//     arr.forEach((el) => {
+//         if (el.toString().includes(target)) {
+//             result.push(el)
+//         }
+//     })
+//
+//     return result
+// };
+
 
 
 console.log(filterLucky([71, 9907, 69], 7), [71, 9907])
