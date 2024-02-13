49a25ad1cc551be8d66e6f10334af37688e7d5f2
AntonioDorosh <antondoroshenko92@gmail.com>
2024-02-13 19:27:04 +0200

    refactor

--- 7kyu/arrayMash.js
+++ 7kyu/arrayMash.js
@@ -5,12 +5,16 @@ const arr2 = ['a', 'b', 'c'];
 const arrayMash = (arr1, arr2) => {
     let result = [];
 
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i], arr2[i])
-    }
+    // for (let i = 0; i < arr1.length; i++) {
+    //     result.push(arr1[i], arr2[i])
+    // }
+
+    arr1.forEach((item, index) => {
+        result.push(item, arr2[index])
+    })
 
     return result
 };
 
 
-console.log(arrayMash(arr1, arr2)) // [1, 'a', 2, 'b', 'c', 3]
+console.log(arrayMash(arr1, arr2)) // [1, 'a', 2, 'b', 3, 'c']
